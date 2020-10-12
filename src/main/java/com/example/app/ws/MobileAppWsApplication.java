1eee5576992ed73df0accef1a78c196285120392
Andres Velasquez <Alvaro.Pineda@aeris.net>
2020-10-12 17:10:51 -0500

    fix: add AppProperties as a java Bean

--- src/main/java/com/example/app/ws/MobileAppWsApplication.java
+++ src/main/java/com/example/app/ws/MobileAppWsApplication.java
@@ -1,5 +1,6 @@
 package com.example.app.ws;
 
+import com.example.app.ws.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -21,5 +22,10 @@ public class MobileAppWsApplication {
     @Bean
     public SpringApplicationContext springApplicationContext() {
         return new SpringApplicationContext();
+    }
+
+    @Bean(name = "AppProperties")
+    public AppProperties appProperties() {
+        return new AppProperties();
     }
 }
